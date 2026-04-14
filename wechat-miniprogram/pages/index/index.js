68bbb3de7a72bc35dc72c74f32c2c9e943e484f6
panyong <py_world@163.com>
2026-04-14 09:36:54 +0800

    feat: add wechat index page fetching tracks from backend

---
+++ wechat-miniprogram/pages/index/index.js
@@ -0,0 +1,27 @@
+const api = require('../../utils/api.js')
+
+Page({
+  data: {
+    tracks: [],
+    rankColors: ['#ff4d4f', '#ff7a45', '#ffa940']
+  },
+
+  onLoad() {
+    this.loadTracks()
+  },
+
+  loadTracks() {
+    api.getTracks().then(tracks => {
+      this.setData({ tracks })
+    }).catch(err => {
+      wx.showToast({ title: '加载失败', icon: 'none' })
+    })
+  },
+
+  goToTrack(e) {
+    const trackId = e.currentTarget.dataset.id
+    wx.navigateTo({
+      url: `/pages/track/track?id=${trackId}`
+    })
+  }
+})
